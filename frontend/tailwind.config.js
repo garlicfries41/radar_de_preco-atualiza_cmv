6087f2900d1588171a428f77f8409083f6316b2d
garlicfries41 <alissonkalil.dev@gmail.com>
2026-02-09 11:41:19 -0300

    Fix: Atualiza Node.js e config do Tailwind para deploy

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,6 +10,7 @@ export default {
                 // We will add custom colors here later based on "HelloBonsai" design system
                 primary: '#10B981', // Emerald 500 as placeholder
                 secondary: '#3B82F6', // Blue 500
+                background: '#F3F4F6', // Gray 100
             },
         },
     },
