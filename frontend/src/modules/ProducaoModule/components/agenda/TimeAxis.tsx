11916fb2f5b183a6080c006f9c62b27c2a496648
garlicfries41 <alissonkalil.dev@gmail.com>
2026-03-10 06:41:46 -0300

    feat: add TimeAxis component

---
+++ frontend/src/modules/ProducaoModule/components/agenda/TimeAxis.tsx
@@ -0,0 +1,22 @@
+const START_HOUR = 5;
+const END_HOUR = 19;
+const PIXELS_PER_MINUTE = 1.5;
+
+export function TimeAxis() {
+    const hours = Array.from({ length: END_HOUR - START_HOUR }, (_, i) => START_HOUR + i);
+    return (
+        <div className="w-12 flex-shrink-0 border-r border-gray-200 bg-white relative" style={{ marginTop: '49px' }}>
+            {hours.map(h => (
+                <div
+                    key={h}
+                    className="absolute w-full text-right pr-2"
+                    style={{ top: `${(h - START_HOUR) * 60 * PIXELS_PER_MINUTE}px` }}
+                >
+                    <span className="text-[10px] text-gray-400 leading-none">
+                        {String(h).padStart(2, '0')}h
+                    </span>
+                </div>
+            ))}
+        </div>
+    );
+}
